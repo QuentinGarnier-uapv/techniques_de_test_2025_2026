5f417d7746d31d64b7a195b644fa50609e378ee1
Quentin <109299545+QuentinGrnr@users.noreply.github.com>
2025-11-04 18:18:24 +0100

    add test_api file (client + elementary test_fct)

---
+++ tests/test_api.py
@@ -0,0 +1,25 @@
+import pytest
+from triangulator_app import app
+
+# REMEMBER => use monkeypatch pour fake les retours des methodes du PSM
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as c:
+        yield c
+
+def test_triangulation_success_binary(client, monkeypatch):
+    assert True
+
+def test_invalid_uuid_returns_400(client):
+    assert True
+
+def test_pointset_not_found_returns_404(client, monkeypatch):
+    assert True
+
+def test_triangulation_internal_error_returns_500(client, monkeypatch):
+    assert True
+
+def test_pointset_manager_unavailable_returns_503(client, monkeypatch):
+    assert True
