db06726c140d408f63b96e72e8296b3d1e368556
Quentin <109299545+QuentinGrnr@users.noreply.github.com>
2025-11-04 18:32:11 +0100

    add test_invalid_uuid_returns_400

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 import pytest
 from triangulator_app import app
 
+
 # REMEMBER => use monkeypatch pour fake les retours des methodes du PSM
 
 @pytest.fixture
@@ -9,17 +10,26 @@ def client():
     with app.test_client() as c:
         yield c
 
+
 def test_triangulation_success_binary(client, monkeypatch):
     assert True
 
+
 def test_invalid_uuid_returns_400(client):
-    assert True
+    response = client.get("/triangulation/***invalid-uuid***")
+    assert response.status_code == 400
+    assert response.get_json() == {
+        "code": "TRIANGULATION_FAILED",
+        "message": "Triangulation could not be computed for the given point set."
+    }
 
 def test_pointset_not_found_returns_404(client, monkeypatch):
     assert True
 
+
 def test_triangulation_internal_error_returns_500(client, monkeypatch):
     assert True
+
 
 def test_pointset_manager_unavailable_returns_503(client, monkeypatch):
     assert True
--- triangulator_app.py
+++ triangulator_app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 tri = Triangulator()
 
 @app.route("/triangulation/<pointSetId>", methods=["GET"])
-def get_triangulation(pointSetId):
-    return jsonify({"code": "", "message": ""}), 0
+def get_triangulation(pointSetId: str):
+    return jsonify({"code": "", "message": pointSetId}), 0
 
 if __name__ == "__main__":
     app.run(debug=True)
