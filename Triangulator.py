ddea4c4f4ac57df4cec848e075dedb9a577b7fe0
Quentin <109299545+QuentinGrnr@users.noreply.github.com>
2025-11-04 17:55:55 +0100

    Create base triangulator structure + base api structure

---
+++ Triangulator.py
@@ -0,0 +1,9 @@
+class Triangulator:
+    def __init__(self):
+        pass
+
+    def triangulate(self, pointSetId):
+        return []
+
+    def get_point_set(self, pointSetId):
+        return None
---
+++ triangulator_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, Response
+import struct
+import uuid
+from Triangulator import Triangulator
+
+app = Flask(__name__)
+tri = Triangulator()
+
+@app.route("/triangulation/<pointSetId>", methods=["GET"])
+def get_triangulation(pointSetId):
+    return jsonify({"code": "", "message": ""}), 0
+
+if __name__ == "__main__":
+    app.run(debug=True)
