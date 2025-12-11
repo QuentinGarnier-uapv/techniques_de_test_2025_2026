3933b67fdbb91de3db10cf1fc938233e675100d5
Quentin <232542381+QuentinGarnier-uapv@users.noreply.github.com>
2025-12-11 16:58:31 +0100

    green PointSet

--- models/PointSet.py
+++ models/PointSet.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, field
+import struct
 
 from .Point import Point
+
+# note formatage : < => little-endian (octet de poids faible en premier), I => unsigned int (4 bytes)
 
 
 @dataclass
@@ -9,11 +12,27 @@ class PointSet:
 
     def to_bytes(self) -> bytes:
         """Format: 4 bytes unsigned int count + each point (8 bytes)."""
-        # TODO: Implémenter
-        return b""
+        header = struct.pack("<I", len(self.points))
+        body = b""
+        for p in self.points:
+            body += p.to_bytes()
+        return header + body
 
     @classmethod
-    def from_bytes(cls, data: bytes) -> "PointSet":
+    def from_bytes(self, data: bytes) -> "PointSet":
         """Parse PointSet from bytes."""
-        # TODO: Implémenter
-        raise NotImplementedError("Non implémenté")
+        if len(data) < 4:
+            raise ValueError("Data too short")
+        
+        count = struct.unpack("<I", data[:4])[0]
+        points = []
+        offset = 4
+        
+        for _ in range(count):
+            if offset + 8 > len(data):
+                raise ValueError("Data too short for points")
+            bytePoint = data[offset : offset + 8]
+            points.append(Point.from_bytes(bytePoint))
+            offset += 8
+            
+        return self(points=points)
