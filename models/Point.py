42386721c7e8e89f8b154e53ab3eff265911b1a4
Quentin <232542381+QuentinGarnier-uapv@users.noreply.github.com>
2025-12-11 16:50:20 +0100

    implement Point.py

--- models/Point.py
+++ models/Point.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-
+import struct
+# note formatage : <ff => little-endian (octet de poids faible en premier), 2 floats (32 bits chacun) => perte de précision car en python floats de 64 bits
 
 @dataclass
 class Point:
@@ -8,15 +9,16 @@ class Point:
 
     def to_bytes(self) -> bytes:
         """Serialize as two 4-byte floats (little-endian)."""
-        # TODO: Implémenter la conversion
-        return b""
+        return struct.pack("<ff", self.x, self.y)
 
     @classmethod
     def from_bytes(cls, b: bytes) -> "Point":
         """Deserialize 8 bytes into a Point."""
-        # TODO: Implémenter la lecture
-        # On retourne un point par défaut ou on laisse planter le test proprement
-        raise NotImplementedError("La méthode from_bytes n'est pas encore implémentée")
+        try:
+            x, y = struct.unpack("<ff", b)
+            return cls(x, y)
+        except struct.error as e:
+            raise ValueError("Invalid byte sequence for Point") from e
 
     def to_tuple(self) -> tuple[float, float]:
         return (self.x, self.y)
